3d226eeaecbe52a168fb90ffaf86287533951731
AndrewPPinner <84006152+AndrewPPinner@users.noreply.github.com>
2021-09-28 18:55:16 -0400

    New feature plus CSS
    Styling and added meal type selection

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const app = Vue.createApp({
             instructions: null,
             title: null,
             source: null,
-            show: null
+            show: null,
+            mealType: null
 
         }
     },
@@ -21,6 +22,12 @@ const app = Vue.createApp({
             .get("https://api.spoonacular.com/recipes/random?apiKey=799d622dd61b464ba425097c8c0d5023&tags=vegetarian")
             .then(vResponse => (console.log(vResponse), this.img = vResponse.data.recipes[0].image, this.instructions = vResponse.data.recipes[0].instructions, this.title = vResponse.data.recipes[0].title, this.source = vResponse.data.recipes[0].spoonacularSourceUrl, this.show = true))
             .catch(vE => (console.log(vE)))
+        },
+        randomMeal(mealType) {
+            axios
+            .get("https://api.spoonacular.com/recipes/random?apiKey=799d622dd61b464ba425097c8c0d5023&tags=" + mealType)
+            .then(response => (console.log(response), this.img = response.data.recipes[0].image, this.instructions = response.data.recipes[0].instructions, this.title = response.data.recipes[0].title, this.source = response.data.recipes[0].spoonacularSourceUrl, this.show = true))
+            .catch(e => (console.log(e)))
         }
     }
 })
